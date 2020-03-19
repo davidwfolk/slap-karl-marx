4cc6b08327be02d619f27d622e42b2860a03fcb1
David Folk <davidwfolk@gmail.com>
2020-03-19 16:02:50 -0600

    buttons working,  wet willy hidden

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 
 let Karl = {
   health: 100,
+  dead:
 }
 
 let attackChoices = {
   "slap": 7,
-  "wet-willy": 50,
   "kick": 12,
+  "wetWilly": 50,
 }
 
 
 function karlDamage(playerChoice) {
  let slapDamage = attackChoices[playerChoice]
-console.log(slapDamage);
-// debugger
-//   let karlPoints = ''
-// for (const key in playerChoice) {
-//   if (playerChoice.hasOwnProperty(key)) {
-//     const karlPoints = playerChoice[key].hit;
+ let newKarlHealth = Karl.health - slapDamage
+    Karl.health = newKarlHealth
+    console.log(newKarlHealth);
+ 
+  if (newKarlHealth <= 50) {
+     document.getElementById("btn-wet-willy").classList.remove("hidden")
+  }
+
+    if (Karl.health <= 0) {
     
-//   }
-//   let newKarlHealth = Karl.health - karlPoints
-//   console.log(newKarlHealth);
-// }
-    }
+   document.getElementById("btn-reset").classList.remove("hidden")
+ }
+  }
