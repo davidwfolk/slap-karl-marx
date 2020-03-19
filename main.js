5c32033c37a41048c2736d307bd748ff8e22a5ce
David Folk <davidwfolk@gmail.com>
2020-03-19 15:04:05 -0600

    v2

---
+++ main.js
@@ -0,0 +1,26 @@
+
+let Karl = {
+  health: 100,
+}
+
+let attackChoices = {
+  "slap": 7,
+  "wet-willy": 50,
+  "kick": 12,
+}
+
+
+function karlDamage(playerChoice) {
+ let slapDamage = attackChoices[playerChoice]
+console.log(slapDamage);
+// debugger
+//   let karlPoints = ''
+// for (const key in playerChoice) {
+//   if (playerChoice.hasOwnProperty(key)) {
+//     const karlPoints = playerChoice[key].hit;
+    
+//   }
+//   let newKarlHealth = Karl.health - karlPoints
+//   console.log(newKarlHealth);
+// }
+    }
